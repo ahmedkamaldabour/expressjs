08f7e47243110007e26d831994742cc3e62a6130
dev.dabour <dabourdabour28@gmail.com>
2025-12-18 18:19:15 +0200

    feat: enhance app security with middleware and dependencies
    Added security-related middleware (`helmet`, `xss-clean`, `express-mongo-sanitize`, `hpp`, `express-rate-limit`) for improved request handling and protection against common security vulnerabilities. Updated `package.json` and `package-lock.json` to include new dependencies.

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const mongoose = require('mongoose');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -13,6 +18,41 @@ const apiResponse = require("./app/Helpers/apiResponse");
 const {static} = require("express");
 
 var app = express();
+
+// 1) GLOBAL MIDDLEWARES
+// Set security HTTP headers
+app.use(helmet());
+
+// Limit requests from same API
+const limiter = rateLimit({
+    max: 100,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+// Body parser, reading data from body into req.body
+app.use(express.json({limit: '10kb'}));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitization against XSS
+app.use(xss());
+
+// Prevent parameter pollution
+app.use(
+    hpp({
+        whitelist: [
+            'duration',
+            'ratingsQuantity',
+            'ratingsAverage',
+            'maxGroupSize',
+            'difficulty',
+            'price'
+        ]
+    })
+);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,7 +73,7 @@ app.all('/api/*',
         return apiResponse(res, 404, `Route not found ${req.originalUrl}`);
     });
 
-// catch 404 and forward to error handler
+// catch 404 and forward to the error handler
 app.use('*', function (req, res, next) {
     next(createError(404));
 });
